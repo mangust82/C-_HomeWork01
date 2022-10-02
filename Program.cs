49c9d8dd9729c18e3c12ce12828a60726349f833
Eugen <mangust82@gmail.com>
2022-10-02 15:56:38 +0300

    task06

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ else{ Console.WriteLine($"max = {b}");}*/
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-int max;
+/*int max;
 Console.WriteLine("ВВедите число а");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("ВВедите число b");
@@ -20,4 +20,19 @@ int c = int.Parse(Console.ReadLine());
 if (a > b) { max = a;}
 else{ max = b;}
 if (c > max) { max = c;}
-Console.WriteLine($"max = {max}");
+Console.WriteLine($"max = {max}");*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("ВВедите число");
+int a = int.Parse(Console.ReadLine());
+if ( a == 0) {
+    Console.WriteLine("Это 0");
+    Environment.Exit(0);}
+if ( a % 2 == 0) {Console.WriteLine("Четное");}
+else {Console.WriteLine("нечетное");}
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
