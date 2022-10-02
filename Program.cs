a4d4767b3bb56ea02efd7c0c50951fce2caf9c53
Eugen <mangust82@gmail.com>
2022-10-02 14:59:47 +0300

    task04

--- Program.cs
+++ Program.cs
@@ -8,3 +8,16 @@ int b = int.Parse(Console.ReadLine());
 if (a > b) { Console.WriteLine($"max = {a}");}
 else{ Console.WriteLine($"max = {b}");}*/
 
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int max;
+Console.WriteLine("ВВедите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВедите число b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВедите число c");
+int c = int.Parse(Console.ReadLine());
+if (a > b) { max = a;}
+else{ max = b;}
+if (c > max) { max = c;}
+Console.WriteLine($"max = {max}");
